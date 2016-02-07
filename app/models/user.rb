d5d08976040c192cad6e9b2ad345171c6cf47ce2
Anton Olofsson <anton.olofsson@gmail.com>
2016-02-07 19:05:36 +0100

    Added password to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
     has_many :user_files, dependent: :destroy
     validates :name, presence: true
+    validates :password, presence: true, length: {minimum: 8}
     validates :email, presence: true, uniqueness: true
 end
--- commands.md
+++ commands.md
@@ -12,4 +12,5 @@ rake db:migrate
 rails generate model UserFile name:string data:text comment:text user:references --force
 rails generate controller UserFiles
 
-6.4
+rails generate migration Users password:string
+rake db:migrate
