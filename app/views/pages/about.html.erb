e536dc9b041e8d42e2cf5c1f05a7e15bf37aa9fe
Anton Olofsson <anton.olofsson@gmail.com>
2016-02-07 18:44:57 +0100

    Styling and a new controller for static pages.

---
+++ app/views/pages/about.html.erb
@@ -0,0 +1,1 @@
+<h1>About this site</h1>
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/help'
+
+  get 'pages/about'
+
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
